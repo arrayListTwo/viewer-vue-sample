b3a2c6ed92dbbdefabf96bd145014372b22df158
丁保磊 <1348909544@qq.com>
2024-06-27 19:36:23 +0800

    viewer basic

---
+++ src/components/pdf-viewer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pdf-view" ref="pdfViewer"></div>
+</template>
+<script>
+
+import PdfWebView from '@pdftron/pdfjs-express-viewer'
+
+export default {
+  name: 'pdf-viewer',
+  props: {
+    path: {
+      type: String,
+      default: process.env.BASE_URL
+    }
+  },
+  mounted () {
+    PdfWebView({
+      path: `${this.path}pdf-viewer-lib`,
+      initialDoc: `${process.env.BASE_URL}files/测试pdf文件.pdf`,
+      licenseKey: 'aOWN16k2sFXSOXKDzvBH'
+    }, this.$refs.pdfViewer).then(instance => {
+      const {Core, UI} = instance
+      console.log(Core)
+      console.log(UI)
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pdf-view {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,27 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <el-button>el-button</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pdf-viewer class="viewer-container"></pdf-viewer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import PdfViewer from '@/components/pdf-viewer.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld, PdfViewer
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.viewer-container {
+  height: 600px;
+  width: 90%;
+  margin: 20px auto;
+}
+</style>
