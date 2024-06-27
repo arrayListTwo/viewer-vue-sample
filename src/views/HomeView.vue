b39a7d17700da598733182effd0158bb596563be
丁保磊 <1348909544@qq.com>
2024-06-27 16:07:17 +0800

    el

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <el-button>el-button</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
